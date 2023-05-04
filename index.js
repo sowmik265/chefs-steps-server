31306e72a93f46c8fc3514e5f43ee442a2026c0a
Tanvir Ahamed Sowmik <sowmik265@gmail.com>
2023-05-04 12:41:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefsDetails = require('./Data/chefsdetails.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('food is cooking')
+});
+
+app.get('/chefsdetails', (req, res) => {
+    res.send(chefsDetails)
+})
+
+app.listen(port, () => {
+    console.log(`food is cooking on port :${port}`)
+});
